57adb30587c1df4b25a151ed3e3c1d45a9258cf3
Aharon Moskovitsh <f05485@gmail.com>
2025-11-17 12:31:42 +0200

    Add invoice items collection and total calculation logic

--- API/Entities/Invoice.cs
+++ API/Entities/Invoice.cs
@@ -8,8 +8,18 @@ public class Invoice
     public int InvoiceNumber { get; set; }
     public DateOnly InvoiceDate { get; set; }
     public string? FilePath { get; set; }
+
     public int SupplierId { get; set; }
     public virtual Supplier Supplier { get; set; } = null!;
+
     public int UserId { get; set; }
     public virtual Users User { get; set; } = new();
+
+    public virtual ICollection<InvoiceItem> Items { get; set; } = new List<InvoiceItem>();
+
+    public decimal Total {get; private set; }
+    public void ReCalculateTotal()
+    {
+        Total = Items.Sum(i => i.Price * i.Quantity);
+    }
 }
